3a80b884913e4588d35b88acf20456d968f9f306
alexeychiv <alexius-ch@yandex.ru>
2021-09-24 11:31:13 +0300

    HW 8 - Asynchronous DB quries
    Реализована асинхронная работа с хранилищем истории загрузок

--- app/src/main/java/gb/android/yanweather/App.kt
+++ app/src/main/java/gb/android/yanweather/App.kt
@@ -28,7 +28,7 @@ class App : Application() {
                         HistoryDataBase::class.java,
                         DB_NAME
                     )
-                        .allowMainThreadQueries()
+                        //.allowMainThreadQueries()
                         .addMigrations(MIGRATION_1_2)
                         .addMigrations(MIGRATION_2_3)
                         .addMigrations(MIGRATION_3_4)
@@ -42,19 +42,19 @@ class App : Application() {
 
         private val MIGRATION_1_2: Migration = object : Migration(1, 2) {
             override fun migrate(database: SupportSQLiteDatabase) {
-                database.execSQL(" ALTER TABLE HistoryEntity  RENAME COLUMN condition TO condition2")
+                database.execSQL("ALTER TABLE HistoryEntity RENAME COLUMN condition TO condition2")
             }
         }
 
         private val MIGRATION_2_3: Migration = object : Migration(2, 3) {
             override fun migrate(database: SupportSQLiteDatabase) {
-                database.execSQL(" ALTER TABLE HistoryEntity  RENAME COLUMN condition2 TO condition")
+                database.execSQL("ALTER TABLE HistoryEntity RENAME COLUMN condition2 TO condition")
             }
         }
 
         private val MIGRATION_3_4: Migration = object : Migration(3, 4) {
             override fun migrate(database: SupportSQLiteDatabase) {
-                database.execSQL(" ALTER TABLE HistoryEntity  ADD condition3 TEXT NOT NULL DEFAULT '' ")
+                database.execSQL("ALTER TABLE HistoryEntity ADD condition3 TEXT NOT NULL DEFAULT ''")
             }
         }
     }
